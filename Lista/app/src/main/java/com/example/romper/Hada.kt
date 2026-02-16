0f72cf239656ec06778b54d69934e990ac9afa34
rocket <157631462+RocketEliana@users.noreply.github.com>
2026-02-16 15:06:54 +0100

    now

---
+++ Lista/app/src/main/java/com/example/romper/Hada.kt
@@ -0,0 +1,7 @@
+package com.example.romper
+
+data class Hada(
+    val imagen:Int,
+    val nombre:String
+
+)
---
+++ Lista/app/src/main/java/com/example/romper/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.romper
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.SeekBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.romper.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: Adapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val listaAdas=listOf<Hada>(
+            Hada(R.drawable.loto,"Loto"),
+            Hada(R.drawable.luna,"luna"),
+            Hada(R.drawable.tierra,"tierra")
+        )
+        adapter= Adapter(this,listaAdas)
+        binding.lista.adapter=adapter
+        /*binding.lista.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //el objeto que te devuelve adapter.getItem(position) es efectivamente un objeto de tipo Hada,
+                val hada = adapter.getItem(position)
+                hada?.let { binding.text.text=it.nombre }//asegurate siempre de que controlas los nulos
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }*/
+        //FIJATE LA DIFERENCIA!
+        binding.lista.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+
+            val hada=listaAdas[position]
+            binding.text.text=hada.nombre
+            }
+
+
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+
+
